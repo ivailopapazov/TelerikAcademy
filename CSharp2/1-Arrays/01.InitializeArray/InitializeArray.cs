15145b33161ac0388214bd6eba1d1600b802e446
Ivailo Papazov <ihellfiend@gmail.com>
2013-12-12 13:47:57 +0200

    solved exercise 1 from arrays
    C# Part 2 Lecture 1

---
+++ CSharp2/1-Arrays/01.InitializeArray/InitializeArray.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+class InitializeArray
+{
+    static void Main()
+    {
+        // Allocating array
+        int[] numbers = new int[20];
+
+        // Initializing array
+        for (int i = 0; i < numbers.Length; i++)
+        {
+            numbers[i] = i * 5;
+        }
+
+        // Print array
+        foreach (var number in numbers)
+        {
+            Console.WriteLine(number);
+        }
+    }
+}
