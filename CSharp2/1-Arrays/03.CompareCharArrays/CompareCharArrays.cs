7bedca14a0aa3ae705706d5e1eb96c6b3441e102
Ivailo Papazov <ihellfiend@gmail.com>
2013-12-14 16:22:05 +0200

    solved exercise 3 from arrays

--- CSharp2/1-Arrays/03.CompareCharArrays/CompareCharArrays.cs
+++ CSharp2/1-Arrays/03.CompareCharArrays/CompareCharArrays.cs
@@ -4,31 +4,49 @@ class CompareCharArrays
 {
     static void Main()
     {
-        // User input 
-        Console.Write("Please enter the first string: ");
-        string firstString = Console.ReadLine();
-        Console.Write("Please enter the second string: ");
-        string secondString = Console.ReadLine();
-        
-        // Find the longest string length
-        int length = Math.Max(firstString.Length, secondString.Length);
+        // Var declaration
+        bool isIdentical = true;
 
-        // Remove uppercase, equalize string lengths with spaces, convert to char array 
-        char[] firstArray = firstString.ToLower().PadRight(length).ToCharArray();
-        char[] secondArray = secondString.ToLower().PadRight(length).ToCharArray();
+        // User input 
+        Console.Write("Please enter the first word: ");
+        char[] firstArray = Console.ReadLine().ToLower().ToCharArray();
+        Console.Write("Please enter the second word: ");
+        char[] secondArray = Console.ReadLine().ToLower().ToCharArray();
         
+        // Find the shorter length
+        int length = Math.Min(firstArray.Length, secondArray.Length);
+
         // Compare arrays element wise
-        for (int i = 0; i < firstArray.Length; i++)
+        for (int i = 0; i < length; i++)
         {
             if (firstArray[i] < secondArray[i])
             {
-                Console.WriteLine("{0} is lexicographically first", firstString);
+                Console.WriteLine("{0} is lexicographically first", new string(firstArray));
+                isIdentical = false;
                 break;
             }
             else if (firstArray[i] > secondArray[i])
             {
-                Console.WriteLine("{0} is lexicographically first", secondString);
+                Console.WriteLine("{0} is lexicographically first", new string(secondArray));
+                isIdentical = false;
                 break;
+            }
+        }
+
+        // Compare arrays by length if elements are the same
+        if (isIdentical)
+        {
+            if (firstArray.Length < secondArray.Length)
+            {
+                Console.WriteLine("{0} is lexicographically first", new string(firstArray));
+            }
+            else if (secondArray.Length < firstArray.Length)
+            {
+                Console.WriteLine("{0} is lexicographically first", new string(secondArray));
+            }
+            else
+            {
+                Console.WriteLine("The words are lexicographically the same");
             }
         }
     }
