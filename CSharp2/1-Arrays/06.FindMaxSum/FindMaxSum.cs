20f52d777c10b45c0a8ec1c10250dc082c606c58
Ivailo Papazov <ihellfiend@gmail.com>
2013-12-15 16:55:06 +0200

    solved exercise 6 from arrays

---
+++ CSharp2/1-Arrays/06.FindMaxSum/FindMaxSum.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+class FindMaxSum
+{
+    static void Main()
+    {
+        // Variable Declaration
+        int maxSum = 0;
+
+        // User input
+        Console.Write("Please enter the array size: ");
+        int arrayLength = int.Parse(Console.ReadLine());
+        Console.Write("Please enter the number for K: ");
+        int k = int.Parse(Console.ReadLine());
+        if (k < 1 || k > arrayLength)
+        {
+            Console.WriteLine("Invalid value!");
+            return;
+        }
+        int[] numbers = new int[arrayLength];
+        for (int i = 0; i < numbers.Length; i++)
+        {
+            Console.Write("Please enter number {0}: ", i + 1);
+            numbers[i] = int.Parse(Console.ReadLine());
+        }
+
+        // Sort array
+        Array.Sort(numbers);
+
+        // Get the sum of the last K elements
+        for (int i = numbers.Length - 1; i > numbers.Length - k - 1; i--)
+        {
+            maxSum += numbers[i];
+        }
+
+        // Printing result
+        Console.WriteLine("Maximal sum of {0} elements is equal to {1}", k, maxSum);
+    }
+}
