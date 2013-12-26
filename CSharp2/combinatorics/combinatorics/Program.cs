eb0dffdaaec669d3d069378cfca32c7e92cf611f
Ivailo Papazov <ihellfiend@gmail.com>
2013-12-26 09:32:31 +0200

    delete zip file

---
+++ CSharp2/combinatorics/combinatorics/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace combinatorics
+{
+    class Program
+    {
+        static void Main()
+        {
+            int k = 4;
+            int n = 6;
+
+            int[] elements = new int[k];
+            Variations(elements, 0, n);
+        }
+
+        static void Variations(int[] arr, int index, int stop)
+        {
+            if (index >= arr.Length)
+            {
+                Console.WriteLine(string.Join(", ", arr));
+            }
+            else
+            {
+                for (int i = index; i < stop; i++)
+                {
+                    arr[index] = i + 1;
+                    Variations(arr, index + 1, stop);
+                }
+            }
+        }
+    }
+}
