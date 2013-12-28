5fca399bca4fa9dac0c2416ad701ce6328dd5595
lnxslackware <lnxslackware@gmail.com>
2013-12-28 14:25:29 +0200

    solved exercise 2 from multidimensional arrays

--- CSharp2/2-Multidimensional-Arrays/01.PrintMatrices/PrintMatrices.cs
+++ CSharp2/2-Multidimensional-Arrays/01.PrintMatrices/PrintMatrices.cs
@@ -1,82 +1,130 @@
 ﻿using System;
 
-class PrintMatrices
+namespace Matrices
 {
-    static void Main()
+    public class PrintMatrices
     {
-        // User Input
-        Console.Write("Please enter N: ");
-        int n = int.Parse(Console.ReadLine());
-
-        // Variable Declaration
-        int[,] matrix = new int[n, n];
-        int count = 1;
-
-        // Generate Matrix A
-        for (int j = 0; j < matrix.GetLength(1); j++)
+        static void Main()
         {
-            for (int i = 0; i < matrix.GetLength(0); i++)
-            {
-                matrix[i, j] = count;
-                count++;
-            }
-        }
+            // User Input
+            Console.Write("Please enter N: ");
+            int n = int.Parse(Console.ReadLine());
 
-        // Print Matrix A
-        PrintMatrix(matrix);
+            // Variable Declaration
+            int[,] matrix = new int[n, n];
+            int count = 1;
 
-        // Generate Matrix B
-        count = 1;
-        for (int j = 0; j < matrix.GetLength(1); j++)
-        {
-            for (int i = 0; i < matrix.GetLength(0); i++)
+            // Generate Matrix A
+            for (int j = 0; j < matrix.GetLength(1); j++)
             {
-                matrix[i, j] = count;
-                count++;
-            }
-            if (j + 1 < matrix.GetLength(1))
-            {
-                j++;
-                for (int i = matrix.GetLength(0) - 1; i >= 0; i--)
+                for (int i = 0; i < matrix.GetLength(0); i++)
                 {
                     matrix[i, j] = count;
                     count++;
                 }
             }
-            
-        }
-        PrintMatrix(matrix);
+            PrintMatrix(matrix);
 
-        // Matrix C
-        count = 1;
-        for (int k = matrix.GetLength(0) - 1; k >= 0; k--)
-        {
-            for (int i = k, j = 0; i < matrix.GetLength(0); i++, j++)
-            {
-                matrix[i, j] = count;
-                count++;
-            }
-        }
-        for (int k = 1; k < matrix.GetLength(1); k++)
-        {
-            for (int j = k, i = 0; j < matrix.GetLength(1); j++, i++)
-            {
-                matrix[i, j] = count;
-                count++;
-            }
-        }
-        PrintMatrix(matrix);
-    }
-    static void PrintMatrix(int[,] matrix)
-    {
-        Console.WriteLine(new string('-', matrix.GetLength(0) * 4));
-        for (int i = 0; i < matrix.GetLength(0); i++)
-        {
+            // Generate Matrix B
+            count = 1;
             for (int j = 0; j < matrix.GetLength(1); j++)
             {
-                Console.Write("{0, -4}", matrix[i,j]);
+                for (int i = 0; i < matrix.GetLength(0); i++)
+                {
+                    matrix[i, j] = count;
+                    count++;
+                }
+                if (j + 1 < matrix.GetLength(1))
+                {
+                    j++;
+                    for (int i = matrix.GetLength(0) - 1; i >= 0; i--)
+                    {
+                        matrix[i, j] = count;
+                        count++;
+                    }
+                }
+
             }
-            Console.WriteLine();
+            PrintMatrix(matrix);
+
+            // Matrix C
+            count = 1;
+            for (int k = matrix.GetLength(0) - 1; k >= 0; k--)
+            {
+                for (int i = k, j = 0; i < matrix.GetLength(0); i++, j++)
+                {
+                    matrix[i, j] = count;
+                    count++;
+                }
+            }
+            for (int k = 1; k < matrix.GetLength(1); k++)
+            {
+                for (int j = k, i = 0; j < matrix.GetLength(1); j++, i++)
+                {
+                    matrix[i, j] = count;
+                    count++;
+                }
+            }
+            PrintMatrix(matrix);
+
+            // Matrix D
+            count = 1;
+            int frames = (n + 1) / 2;
+            for (int frame = 0; frame < frames; frame++)
+            {
+                int i = frame;
+                int j = frame;
+                // South Movement
+                while (i < matrix.GetLength(0) - frame)
+                {
+                    matrix[i, j] = count;
+                    count++;
+                    i++;
+                }
+                i--;
+                j++;
+
+                // East Movement
+                while (j < matrix.GetLength(1) - frame)
+                {
+                    matrix[i, j] = count;
+                    count++;
+                    j++;
+                }
+                j--;
+                i--;
+
+                // North Movement
+                while (i >= frame)
+                {
+                    matrix[i, j] = count;
+                    count++;
+                    i--;
+                }
+                i++;
+                j--;
+
+                // West Movement
+                while (j > frame)
+                {
+                    matrix[i, j] = count;
+                    count++;
+                    j--;
+                }
+            }
+            PrintMatrix(matrix);
+        }
+        public static void PrintMatrix(int[,] matrix)
+        {
+            Console.WriteLine(new string('-', matrix.GetLength(0) * 4));
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                {
+                    Console.Write("{0, -4}", matrix[i, j]);
+                }
+                Console.WriteLine();
+            }
         }
     }
 }
---
+++ CSharp2/2-Multidimensional-Arrays/02.MaxSqareSumInMatrix/MaxSqareSumInMatrix.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using Matrices;
+
+class MaxSqareSumInMatrix
+{
+    static void Main()
+    {
+        Console.Write("Please enter rows: ");
+        int N = int.Parse(Console.ReadLine());
+        Console.Write("Please enter cols: ");
+        int M = int.Parse(Console.ReadLine());
+
+        int[,] matrix = new int[N, M];
+
+        for (int i = 0; i < matrix.GetLength(0); i++)
+        {
+            string[] row = Console.ReadLine().Split(' ');
+            for (int j = 0; j < matrix.GetLength(1); j++)
+            {
+                matrix[i, j] = int.Parse(row[j]);
+            }
+        }
+
+        PrintMatrices.PrintMatrix(matrix);
+
+        int squareField = 3;
+        int squareSum = 0;
+        int squareSumBest = int.MinValue;
+        int[,] squareMatrix = new int[squareField, squareField];
+        int[,] bestSquareMatrix = new int[squareField, squareField];
+
+        for (int matrixRow = 0; matrixRow < matrix.GetLength(0) - squareField; matrixRow++)
+        {
+            for (int matrixCol = 0; matrixCol < matrix.GetLength(1) - squareField; matrixCol++)
+            {
+                for (int squareRow = matrixRow; squareRow < matrixRow + squareField; squareRow++)
+                {
+                    for (int squareCol = matrixCol; squareCol < matrixCol + squareField; squareCol++)
+                    {
+                        squareSum += matrix[squareRow, squareCol];
+                        squareMatrix[squareRow - matrixRow, squareCol -  matrixCol] = matrix[squareRow, squareCol];
+                    }
+                }
+                if (squareSum > squareSumBest)
+                {
+                    squareSumBest = squareSum;
+                    bestSquareMatrix = (int[,])squareMatrix.Clone();
+                }
+                squareSum = int.MinValue;
+            }
+        }
+
+        PrintMatrices.PrintMatrix(bestSquareMatrix);
+        Console.WriteLine("Max sum: {0}", squareSumBest);
+
+    }
+}
