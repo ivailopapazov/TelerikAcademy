b5bae68f7f0d0f4734dedc806e91996deab1d390
IvailoPapazov <ihellfiend@gmail.com>
2014-01-05 17:30:31 +0200

    solved exercise 7 from methods

---
+++ CSharp2/3-Methods/07.ReverseNumberDigits/ReverseNumberDigits.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+class ReverseNumberDigits
+{
+    static int ReverseNumber(int number)
+    {
+        string raw = number.ToString();
+        string reversedRaw = string.Empty;
+        for (int i = raw.Length - 1; i >= 0; i--)
+        {
+            reversedRaw += raw[i];
+        }
+
+        return int.Parse(reversedRaw);
+    }
+    static void Main()
+    {
+        Console.Write("Please enter a number: ");
+        int number = int.Parse(Console.ReadLine());
+
+        int reversedNumber = ReverseNumber(number);
+
+        Console.WriteLine(reversedNumber);
+    }
+}
