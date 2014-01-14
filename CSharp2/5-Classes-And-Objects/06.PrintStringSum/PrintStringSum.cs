51dac5f93ec5c987aef53564b5f1b6d4d80535ff
IvailoPapazov <ihellfiend@gmail.com>
2014-01-14 10:29:56 +0200

    solved exercise 6 from objects

--- CSharp2/5-Classes-And-Objects/06.PrintStringSum/PrintStringSum.cs
+++ CSharp2/5-Classes-And-Objects/06.PrintStringSum/PrintStringSum.cs
@@ -1,30 +1,20 @@
 ﻿using System;
 
-class Program
+class PrintStringSum
 {
-    public class StringSum
-    {
-        string[] numbers;
-        int sum;
-
-        public StringSum(string inputString)
-        {
-            numbers = inputString.Split(' ');
-
-            foreach (var number in numbers)
-            {
-                sum += int.Parse(number);
-            }
-        }
-
-        public override string ToString()
-        {
-
-
-            return 
-        }
-    }
     static void Main()
     {
+        // User input
+        Console.Write("Please enter a string of numbers separated by space: ");
+        string input = Console.ReadLine();
+
+        // Create StringSum object
+        StringSum numbers = new StringSum(input);
+
+        // Calculate sum
+        int sum = numbers.CalculateSum();
+
+        // Print result
+        Console.WriteLine(sum);
     }
 }
---
+++ CSharp2/5-Classes-And-Objects/06.PrintStringSum/StringSum.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+public class StringSum
+{
+    string inputString;
+    int sum;
+
+    public StringSum(string inputString)
+    {
+        this.inputString = inputString;
+    }
+
+    public int CalculateSum()
+    {
+        // Split the string into array of string numbers
+        string[] stringNumbers = inputString.Split(' ');
+
+        // Parse numbers and add to sum
+        foreach (var number in stringNumbers)
+        {
+            sum += int.Parse(number);
+        }
+
+        // Return sum
+        return sum;
+    }
+}
