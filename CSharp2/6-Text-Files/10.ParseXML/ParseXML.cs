1f93eb2d052f4a256b280b30a3538fe431d57864
IvailoPapazov <ihellfiend@gmail.com>
2014-01-15 20:41:05 +0200

    solving exercise 10 from text-files

---
+++ CSharp2/6-Text-Files/10.ParseXML/ParseXML.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using System.Text.RegularExpressions;
+
+class ParseXML
+{
+    // Path constant
+    const string FilePath = @"../../input.xml";
+    static void Main()
+    {
+        // Variable declarations
+        char currentChar = new char();
+        StringBuilder container = new StringBuilder();
+
+        // Get xml content
+        string content = File.ReadAllText(FilePath);
+
+        for (int i = 0; i < content.Length; i++)
+        {
+            // Get current char
+            currentChar = content[i];
+
+            // Move index if xml tag begins
+            if (currentChar == '<')
+            {
+                i = content.IndexOf('>', i);
+            }
+            else
+            {
+                container.Append(currentChar);
+            }
+        }
+
+        Console.WriteLine(container);
+    }
+}
