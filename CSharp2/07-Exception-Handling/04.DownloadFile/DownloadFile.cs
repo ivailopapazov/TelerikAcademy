d1943d78dec73b52e874672d5604ec7fd435df1e
IvailoPapazov <ihellfiend@gmail.com>
2014-01-16 22:57:21 +0200

    solved exercise 4 from exception-handling

---
+++ CSharp2/07-Exception-Handling/04.DownloadFile/DownloadFile.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Net;
+
+class DownloadFile
+{
+    static void Main()
+    {
+        // User input
+        Console.Write("Enter download link: ");
+        string url = Console.ReadLine();
+
+        // Create WebClient object
+        WebClient link = new WebClient();
+        
+        // Try download
+        try
+        {
+            // Get file extension
+            string ext = url.Substring(url.LastIndexOf('.'));
+
+            // Download file
+            link.DownloadFile(url, @"../../download" + ext);
+        }
+        catch (ArgumentOutOfRangeException)
+        {
+            Console.WriteLine("Invalid download URL");
+        }
+        catch (ArgumentNullException exc)
+        {
+            Console.WriteLine(exc.Message);
+        }
+        catch (WebException exc)
+        {
+            Console.WriteLine(exc.Message);
+        }
+        catch (NotSupportedException exc)
+        {
+            Console.WriteLine(exc.Message);
+        }
+        finally
+        {
+            // Free web resource
+            link.Dispose();
+        }
+    }
+}
