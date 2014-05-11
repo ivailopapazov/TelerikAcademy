a0a8c5df5b1a84bd6b015f8a105af377042b21f6
IvailoPapazov <ihellfiend@gmail.com>
2014-05-11 16:00:39 +0300

    Edited homework-1 KPK

--- KPK/1-Code-Formatting/code.js
+++ KPK/1-Code-Formatting/code.js
@@ -41,12 +41,12 @@ function PopTip() {
 			pX = window.innerWidth - 150;
 		}
 
-	 	theLayer.left = pX + 10;
-	 	theLayer.top = pY + 15;
+		theLayer.left = pX + 10;
+		theLayer.top = pY + 15;
 		theLayer.visibility = 'show';
 	} 
 	else {
-   		theLayer = eval('document.all[\'ToolTip\']');
+		theLayer = eval('document.all[\'ToolTip\']');
 		if (theLayer) {
 			pX = event.x - 5;
 			pY = event.y;
@@ -79,39 +79,39 @@ function HideTip() {
 function HideMenu1() {
 	if (b == "Netscape") {
 		document.layers['menu1'].visibility = 'hide';
- 	}
- 	else {
- 		document.all['menu1'].style.visibility = 'hidden';
- 	}
+	}
+	else {
+		document.all['menu1'].style.visibility = 'hidden';
+	}
 }
 
 function ShowMenu1() {
 	if (b == "Netscape") {
-	    theLayer = eval('document.layers[\'menu1\']');
-	    theLayer.visibility = 'show';
+		theLayer = eval('document.layers[\'menu1\']');
+		theLayer.visibility = 'show';
 	}
 	else {
-	    theLayer = eval('document.all[\'menu1\']');
-    	theLayer.style.visibility = 'visible';
-   	}
+		theLayer = eval('document.all[\'menu1\']');
+		theLayer.style.visibility = 'visible';
+	}
 }
 
 function HideMenu2() {
-    if (b == "Netscape") {
-    	document.layers['menu2'].visibility = 'hide';
- 	}
- 	else {
- 		document.all['menu2'].style.visibility = 'hidden';
+	if (b == "Netscape") {
+		document.layers['menu2'].visibility = 'hide';
+	}
+	else {
+		document.all['menu2'].style.visibility = 'hidden';
 	}
 }
 
 function ShowMenu2() {
 	if (b == "Netscape") {
-	    theLayer = eval('document.layers[\'menu2\']');
+		theLayer = eval('document.layers[\'menu2\']');
 		theLayer.visibility = 'show';
-	} 
+	}
 	else {
-	    theLayer = eval('document.all[\'menu2\']');
+		theLayer = eval('document.all[\'menu2\']');
 		theLayer.style.visibility = 'visible';
 	}
 } // fostata
