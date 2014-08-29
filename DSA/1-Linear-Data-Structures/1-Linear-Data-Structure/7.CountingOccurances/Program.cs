67e9a58d8d28c82490f1b0e07faf0242551f0c27
IvailoPapazov <ihellfiend@gmail.com>
2014-08-29 22:09:13 +0300

    Added more Linear-Data-Structures homework

---
+++ DSA/1-Linear-Data-Structures/1-Linear-Data-Structure/7.CountingOccurances/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _7.CountingOccurances
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> sequence = new List<int>();
+            string entry = Console.ReadLine();
+            while (entry != string.Empty)
+            {
+                sequence.Add(int.Parse(entry));
+                entry = Console.ReadLine();
+            }
+
+            Dictionary<int, int> occurances = new Dictionary<int, int>();
+            for (int i = 0; i < sequence.Count; i++)
+            {
+                if (!occurances.ContainsKey(sequence[i]))
+                {
+                    occurances[sequence[i]] = 0;
+                }
+
+                occurances[sequence[i]]++;
+            }
+
+            foreach (KeyValuePair<int, int> item in occurances)
+            {
+                Console.WriteLine("{0} -> {1}", item.Key, item.Value);
+            }
+        }
+    }
+}
---
+++ DSA/1-Linear-Data-Structures/1-Linear-Data-Structure/9.QueueSequence/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _9.QueueSequence
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter N: ");
+            int N = int.Parse(Console.ReadLine());
+            Queue<int> queue = new Queue<int>();
+
+            queue.Enqueue(N);
+            List<int> resultSet = new List<int>();
+            for (int i = 0; i < 50; i++)
+            {
+                int currentElement = queue.Dequeue();
+
+                queue.Enqueue(currentElement + 1);
+                queue.Enqueue(currentElement * 2 + 1);
+                queue.Enqueue(currentElement + 2);
+
+                resultSet.Add(currentElement);
+            }
+
+            Console.WriteLine(string.Join(", ", resultSet));
+        }
+    }
+}
